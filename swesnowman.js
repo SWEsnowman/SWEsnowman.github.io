ef4d025d295b4959a6787ba7cc12e617039aaaad
SWEsnowman <100440384+SWEsnowman@users.noreply.github.com>
2022-10-12 21:52:29 -0400

    Update swesnowman.js

--- swesnowman.js
+++ swesnowman.js
@@ -1,9 +1,9 @@
-const header = document.getElementsByClassName("intro");
+const header = document.querySelector(".intro");
 const html = `
 <h1>Olof Persson</h1>
 <p>I am a student at the Stevens Institute of Technology studying Computer Science</p>
 
 `
 header.innerHTML = html;
-const background = document.getElementsByClassName("intro");
+const background = document.querySelector(".intro");
 background.style.background = "green";
