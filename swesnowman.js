8c1416215f006ebc0ea9d9cd24ab15a44ae17d85
SWEsnowman <100440384+SWEsnowman@users.noreply.github.com>
2022-10-12 21:44:53 -0400

    Create swesnowman.js

---
+++ swesnowman.js
@@ -0,0 +1,9 @@
+const header = document.getElementsByClassName("intro");
+const html = `
+<h1>Olof Persson</h1>
+<p>I am a student at the Stevens Institute of Technology studying Computer Science</p>
+
+`
+header.innerHTML = html;
+const background = document.getElementsByClassName("intro");
+background.style.background = "green";
