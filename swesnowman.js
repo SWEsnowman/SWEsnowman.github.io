0dd478cea14be4ca928e632d2cfef8568e462619
SWEsnowman <100440384+SWEsnowman@users.noreply.github.com>
2022-10-27 13:02:27 -0400

    adding all the different resume sections

--- swesnowman.js
+++ swesnowman.js
@@ -1,9 +1,10 @@
 const header = document.querySelector(".intro");
-const html = `
+const intro = `
 <h1>Olof Persson</h1>
 <p>I am a student at the Stevens Institute of Technology studying Computer Science</p>
 
 `
-header.innerHTML = html;
-const background = document.querySelector("h1");
-background.style.color = "green";
+header.innerHTML = intro;
+header.querySelector("h1").style.textAlign = "center";
+header.querySelector("p").style.textAlign = "center";
+
